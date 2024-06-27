34fab1d696b4acabb7459b6974b2116ffb48907e
kahmoh <joshmcfadden17@gmail.com>
2024-06-27 21:37:17 +0100

    initial project setu[

---
+++ resume-builder/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import React from "react";
+import ResumePage from "./views/resume-page.jsx";
+import './App.css'
+
+function App() {
+    return (
+        <ResumePage></ResumePage>
+    )
+}
+
+export default App
---
+++ resume-builder/src/views/resume-page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {useState} from "react";
+
+function ResumePage() {
+    return <div className= 'resumePage'>hi</div>
+}
+
+export default ResumePage
