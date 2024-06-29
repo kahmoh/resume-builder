d05315a31ecc4345204a596f6d8bb6110ee53723
kahmoh <joshmcfadden17@gmail.com>
2024-06-29 21:21:44 +0100

    Panel.jsx: create panel component

--- resume-builder/src/App.jsx
+++ resume-builder/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import React from "react";
 import ResumePage from "./views/resume-page.jsx";
 import './App.css'
+import InformationForm from "./views/information-form.jsx";
 
 function App() {
     return (
-        <ResumePage></ResumePage>
+        <div className={'content'}>
+            <InformationForm></InformationForm>
+            <ResumePage></ResumePage>
+        </div>
     )
 }
 
